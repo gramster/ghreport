2391a83b148c3d6b54814ebf55ff2b9c2aee716b
Graham Wheeler <>
2026-04-21 12:32:15 -0700

    Remove unused watch import in Insights.vue

--- ghreport/dashboard/frontend/src/views/Insights.vue
+++ ghreport/dashboard/frontend/src/views/Insights.vue
@@ -96,7 +96,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue'
+import { ref, computed } from 'vue'
 import axios from 'axios'
 import { useDateRangeStore } from '@/stores/dateRange'
 
